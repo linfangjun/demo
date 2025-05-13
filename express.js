aed894c15cbabb6bde9a233d5a333a0cb487365d
linfangjun <2239885266@qq.com>
2025-05-13 08:53:00 +0800

    修改express.js
    更新了电话号码

--- express.js
+++ express.js
@@ -8,5 +8,5 @@ function Student(name,sex,age,telephone,address){
 Student.prototype.getInfo=function(){
 	return this.name+','+this.sex+','+this.age+','+this.telephone+','+this.address;
 };
-var student = new Student('林放军','先生','20','13135119207','湖南岳阳');
+var student = new Student('林放军','先生','20','18908405052','湖南岳阳');
 console.log(student.getInfo());
